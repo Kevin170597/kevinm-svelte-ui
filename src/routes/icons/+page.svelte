7389067e6335c8e0200c3265fcb6af26f18f2c9f
Kevin Mendoza <kevin170597@gmail.com>
2024-01-18 16:45:49 -0300

    svg icons

--- src/routes/icons/+page.svelte
+++ src/routes/icons/+page.svelte
@@ -24,5 +24,30 @@
         <Icon iconName="moon" fill="white" size={26} />
         <Icon iconName="search" fill="white" size={26} />
         <Icon iconName="logout" fill="white" size={26} />
+        <Icon iconName="whatsapp" fill="white" size={26} />
+        <Icon iconName="instagram" fill="white" size={26} />
+        <Icon iconName="facebook" fill="white" size={26} />
+        <Icon iconName="twitter" fill="white" size={26} />
+        <Icon iconName="github" fill="white" size={26} />
+        <Icon iconName="linkedin" fill="white" size={26} />
+        <Icon iconName="reddit" fill="white" size={26} />
+        <Icon iconName="threads" fill="white" size={26} />
+        <Icon iconName="next" fill="white" size={26} />
+        <Icon iconName="previous" fill="white" size={26} />
+        <Icon iconName="message" fill="white" size={26} />
+        <Icon iconName="burger" fill="white" size={26} />
+        <Icon iconName="filter" fill="white" size={26} />
+        <Icon iconName="download" fill="white" size={26} />
+        <Icon iconName="upload" fill="white" size={26} />
+        <Icon iconName="pause" fill="white" size={26} />
+        <Icon iconName="share" fill="white" size={26} />
+        <Icon iconName="bell" fill="white" size={26} />
+        <Icon iconName="like" fill="white" size={26} />
+        <Icon iconName="dislike" fill="white" size={26} />
+        <Icon iconName="location" fill="white" size={26} />
+        <Icon iconName="microphone" fill="white" size={26} />
+        <Icon iconName="play" fill="white" size={26} />
+        <Icon iconName="user" fill="white" size={26} />
+        <Icon iconName="pin" fill="white" size={26} />
     </div>
 </div>
